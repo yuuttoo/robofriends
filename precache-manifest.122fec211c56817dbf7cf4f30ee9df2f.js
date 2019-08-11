a2ccc4276b71c342837638d1e67fd7ebf7abc76b
yuuto <yuuttoo@gmail.com>
2019-08-11 23:23:03 +0800

    Updates

---
+++ precache-manifest.122fec211c56817dbf7cf4f30ee9df2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdc80645c557ba0ef62fd9fb8f117f3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "8706f12e8f22ccfc9194",
+    "url": "/robofriends/static/css/main.2fe202c6.chunk.css"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/robofriends/static/js/2.bae3d14d.chunk.js"
+  },
+  {
+    "revision": "8706f12e8f22ccfc9194",
+    "url": "/robofriends/static/js/main.5b942078.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
